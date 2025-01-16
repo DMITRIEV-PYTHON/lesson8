97e3fc270fcc8063e68040f477f588fbf0392980
DMITRIEV-PYTHON <dmitriev.python@gmail.com>
2025-01-17 00:21:17 +0300

    16/01

---
+++ module_8.1.py
@@ -0,0 +1,29 @@
+# Домашнее задание по уроку "Try и Except".
+# Задание "Программистам всё можно":
+# Пора разрушать шаблоны привычного нам Python! Вот вас раздражает, что мы не можем сложить число(int)
+# и строку(str)? Давайте исправим это недоразумение!
+# Реализуйте следующую функцию:
+# add_everything_up, будет складывать числа(int, float) и строки(str)
+# Описание функции:
+# add_everything_up(a, b) принимает a и b, которые могут быть как числами(int, float), так и строками(str).
+# TypeError - когда a и b окажутся разными типами (числом и строкой), то возвращать строковое представление
+# этих двух данных вместе (в том же порядке). Во всех остальных случаях выполнять стандартные действия.
+
+def add_everything_up(a, b):
+    resault = 0
+    try:
+        resault = round((a + b), 3)
+        return resault
+    except TypeError:
+        if type(a) == int or type(a) == float and type(b) == str:  # a - число, b - строка
+            resault = str(a) + b
+        elif type(a) == str and type(b) == int or type(b) == float:  # a - строка, b - число
+            resault = a + str(b)
+    return resault
+
+
+print(add_everything_up(123.456, 'строка'))
+
+print(add_everything_up('яблоко', 4215))
+
+print(add_everything_up(123.456, 7))
